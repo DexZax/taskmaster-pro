e619eee0b429c8a2aacb0df362584fbd0b137577
Dexter Zacour <cdzacour@hotmail.com>
2022-06-03 18:48:21 -0400

    final touches

--- assets/js/script.js
+++ assets/js/script.js
@@ -130,7 +130,7 @@ $("#task-form-modal").on("shown.bs.modal", function () {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-save").click(function () {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -162,15 +162,22 @@ $(".card .list-group").sortable({
   helper: "clone",
   activate: function (event) {
     console.log("activate", this);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function (event) {
     console.log("deactivate", this);
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function (event) {
     console.log("over", event.target);
+    $(event.target).addClass("dropover-active");
+
   },
   out: function (event) {
     console.log("out", event.target);
+    $(event.target).removeClass("dropover-active");
   },
 
   update: function (event) {
@@ -206,9 +213,11 @@ $("#trash").droppable({
   },
   over: function(event, ui) {
     console.log("over");
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event, ui) {
     console.log("out");
+    $(".bottom-trash").removeClass("bottom-trash-active");
   }
 });
 
@@ -233,8 +242,17 @@ $("#remove-tasks").on("click", function () {
     tasks[key].length = 0;
     $("#list-" + key).empty();
   }
+
+  setInterval(function () {
+    $(".card .list-group-item").each(function(index, el) {
+      auditTask(el);
+    });
+  }, (1000 * 60) * 30);
+
   saveTasks();
 });
+
+
 
 // load tasks for the first time
 loadTasks();
